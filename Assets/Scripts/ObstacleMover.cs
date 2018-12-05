8fba95bb0d1cff8b861db4f450b03f68c488ca35
Ethan Bruins <brudut93@gmail.com>
2018-12-05 15:21:51 +0000

    working on the game manager

---
+++ Assets/Scripts/ObstacleMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Resetter"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ObstaclePool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePool : MonoBehaviour {
+
+    public static ObstaclePool instance;
+    public GameObject[] obstacle;
+
+    public int amount;
+   
+    public List<GameObject> obstacles;
+	// Use this for initialization
+	void Awake ()
+    {
+        instance = this;
+        obstacles = new List<GameObject>();
+
+        for(int i = 0; i < amount; i++)
+        {
+            GameObject newObstacle = Instantiate(obstacle[Random.Range(0, obstacle.Length)]);
+
+            newObstacle.transform.parent = transform;
+            obstacles.Add(newObstacle);
+            newObstacle.SetActive(false);
+
+        }
+
+
+	}
+	
+
+    void SpawnObstacle(Vector3 pos, float speed)
+    {
+        foreach(GameObject o in obstacles)
+        {
+            if(!o.activeSelf)
+            {
+                o.transform.position = pos;
+                o.GetComponent<ObstacleMover>().speed = speed;
+                o.SetActive(true);
+                break;
+            }
+        }
+    }
+    
+}
